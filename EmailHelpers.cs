d5e1e4f2fe53cb82a3d4bc66315c87e40a38c0b6
Mark Oliver <mosoftwareenterprises@gmail.com>
2020-09-16 20:33:18 +0100

    Add retry on email sending

--- EmailHelpers.cs
+++ EmailHelpers.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Net;
 using System.Net.Mail;
+using Polly;
 
 namespace websitechangenotifier
 {
@@ -8,16 +10,21 @@ namespace websitechangenotifier
         public void SendEmail(string subject, string body)
         {
             SmtpClient smtp = new SmtpClient();
-            {
-                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
-                smtp.UseDefaultCredentials = false;
-                smtp.EnableSsl = true;
-                smtp.Host = "smtp.gmail.com";
-                smtp.Port = 587;
-                smtp.Credentials = new NetworkCredential("mosoftwareenterprisesemails@gmail.com", "kctboibbaivumdev");
-                // send the email
-                smtp.Send("noreply@mosoftwareenterprises.co.uk", "bigmansbro@gmail.com", subject, body);
-            }
+            smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtp.UseDefaultCredentials = false;
+            smtp.EnableSsl = true;
+            smtp.Host = "smtp.gmail.com";
+            smtp.Port = 587;
+            smtp.Credentials = new NetworkCredential("mosoftwareenterprisesemails@gmail.com", "kctboibbaivumdev");
+
+            Policy
+              .Handle<Exception>()
+              .WaitAndRetry(new[]
+              {
+    TimeSpan.FromSeconds(1),
+    TimeSpan.FromSeconds(2),
+    TimeSpan.FromSeconds(3)
+              }).Execute(() => smtp.Send("mosoftwareenterprisesemails+noreply@gmail.com", "bigmansbro@gmail.com", subject, body));
         }
     }
 }
